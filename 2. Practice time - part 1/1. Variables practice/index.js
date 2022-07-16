192d420e7281fa60c5ad726ae1a59174b314f577
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2022-07-17 00:38:34 +0800

    Completed Section 2, lesson 1.

--- 2. Practice time - part 1/1. Variables practice/index.js
+++ 2. Practice time - part 1/1. Variables practice/index.js
@@ -1,7 +1,9 @@
 // Create two variables, firstName and lastName
-
+let firstName = "Tony"
+let lastName = "Stark"
 // Concatenate the two variables into a third variable called fullName
-
+let fullName = firstName + " " + lastName
 // Log fullName to the console
+console.log(fullName)
 
 
