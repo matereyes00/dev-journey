ebd60c901e7f67fd898293af43350ae765370a3d
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2022-07-18 00:02:37 +0800

    Started on Section 3, lessons 1 to 10.

--- 3. Build a Blackjack game/3. If...else conditionals/index.js
+++ 3. Build a Blackjack game/3. If...else conditionals/index.js
@@ -2,3 +2,11 @@ let firstCard = 6
 let secondCard = 9
 let sum = firstCard + secondCard
 
+// blackjack: get exactly 21 (close to 21 but NEVER above 21)
+if (sum < 21) {
+    console.log("Do you want to draw a new card?")
+} else if (sum === 21) {
+    console.log("Woo! You've got blackjack")
+} else if (sum > 21) {
+    console.log("You're out of the game!")
+}
--- 3. Build a Blackjack game/7. Add the hasBlackJack variable/index.js
+++ 3. Build a Blackjack game/7. Add the hasBlackJack variable/index.js
@@ -2,12 +2,19 @@ let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard
 
+let hasBlackJack = false //default, udh blackjack
+
 if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game! 😭")
 }
 
+// variable keeps track of the state of the game
+// variable reflects whether or not user has blackjack
+
 // CASH OUT!
+console.log(hasBlackJack)
