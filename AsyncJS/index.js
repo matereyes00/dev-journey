50d70751f15fe244cc0d4e77a1a9eb8aeacb15bb
matereyes00 <martina.therese.reyes@obf.ateneo.edu>
2022-07-23 02:36:48 +0800

    Introduction to synchronous vs asynchronous in JS context. Talked about setTimeOut async function.

---
+++ AsyncJS/index.js
@@ -0,0 +1,21 @@
+// synchronous - prints it 1 by one. If one process gets stuck, everything else is affected.
+console.log(" I ");
+console.log(" eat ");
+
+// asynchronous fxn
+//setTimer: runs a function after a certian amount of time
+// args, time (ms)
+setTimeout(()=>{
+    console.log(" ice cream ");
+}, 4000)
+
+console.log(" with a ");
+console.log(" spoon ");
+
+// what if u wanna move the word spoon
+// console.log(" I ");
+// console.log(" eat ");
+// console.log(" spoon ");
+// console.log(" ice cream ");
+// console.log(" with a ");
+
