b75455fa9c7946f0a5d3ff675d11563079ccaa38
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2022-12-26 23:43:20 +0800

    Did lessons on data types, working with strings and numbers in php.

--- site.php
+++ site.php
@@ -22,7 +22,53 @@
         echo "He was $characterAge years old <br>";
         echo "He really liked the name $characterName <br>";
         echo "But he didn't like being $characterAge <br>";
+
+        echo "<h1> Data Types </h1>";
+        echo "<hr>";
+        // string
+        $phrase = "to be or not to be";
+        // numbers: whole and decimal (float)
+        $age = 22;
+        $gpa = 3.23;
+        // booleans
+        $isMale = false;
+        echo $phrase;
+
+        echo "<h1> Using Strings </h1>";
+        echo "<hr>";
     
+        // storing inside variable
+        $phrase2 = "Giraffe Academy <br>";
+        echo $phrase2;
+
+        // using functions to do special things with strings in the program
+        // echo strtolower($phrase2);
+        echo strtoupper($phrase2);
+        echo strlen($phrase2);
+        echo $phrase2[0];
+        echo str_replace("Giraffe", "Panda", $phrase2);
+        echo substr($phrase2, 8); // index where we want to grab substring from
+        echo substr($phrase2, 8, 3); // index where we want to grab substring from
+
+        echo "<h1> Working with Numbers </h1>";
+        echo "<hr>";
+        echo 10 % 3;
+        echo "<br>";
+        $num = 30;
+        $num += 24;
+        echo "$num <br>";
+        // more advanced, functions to get stuff like square roots n all
+        echo abs(-102);
+        echo "<br>";
+        echo max(2,10);
+        echo "<br>";
+        echo min(2,10);
+        echo "<br>";
+        echo round(2.10);
+        echo "<br>";
+        echo ceil(2.10);
+        echo "<br>";
+        echo floor(2.10);
     ?>
 </body>
 </html>
